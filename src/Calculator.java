b76ce3ada35eb4a23b2802f20a98a91e97db433d
josephkwon <devel.kwon@gmail.com>
2018-07-31 15:40:39 +0900

    기존 결과값에 계속해서 연산 가능하게 구현 - 계산기 기본 기능 구현 완료

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,24 +7,37 @@ public class Calculator {
 		int first = scanner.nextInt();
 		System.out.println(first);
 		
-		System.out.println("사칙연산 기호 : ");
-		String exp = scanner.next();
-		System.out.println(exp);
-		
-		System.out.println("두 번째 입력 값 : ");
-		int second = scanner.nextInt();
-		System.out.println(second);
-		
-		if("+".equals(exp)) {
-			System.out.println("덧셈 : " + (first + second));
-		} else if ("-".equals(exp)) {
-			System.out.println("뺄셈 : " + (first - second));
-		} else if ("*".equals(exp)) {
-			System.out.println("곱셈 : " + (first * second));
-		} else if ("/".equals(exp)) {
-			System.out.println("나눗셈 : " + (first / second));
-		} else {
-			System.out.println("사칙연산 기호가 아닙니다.");
+		int result = first;
+		while (true) {
+			System.out.println("사칙연산 기호 : ");
+			String exp = scanner.next();
+			System.out.println(exp);
+			
+			if ("quit".equals(exp)) {
+				System.out.println("최종 결과 값 : " + result);
+				break;
+			}
+			
+			System.out.println("두 번째 입력 값 : ");
+			int second = scanner.nextInt();
+			System.out.println(second);
+			
+			if("+".equals(exp)) {
+				result = result + second;
+				System.out.
+				println("덧셈 : " + result);
+			} else if ("-".equals(exp)) {
+				result = result - second;
+				System.out.println("뺄셈 : " + result);
+			} else if ("*".equals(exp)) {
+				result = result * second;
+				System.out.println("곱셈 : " + result);
+			} else if ("/".equals(exp)) {
+				result = result / second;
+				System.out.println("나눗셈 : " + result);
+			} else {
+				System.out.println("사칙연산 기호가 아닙니다.");
+			}
 		}
 	}
 }
