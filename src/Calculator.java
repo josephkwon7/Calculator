e18af971b98c4ae3d4521daea1eca10297f26349
josephkwon <devel.kwon@gmail.com>
2018-07-31 19:41:00 +0900

    문자열로 입력을 받아 한번에 계산 구현 - 본인 작성 코드

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,70 +1,37 @@
 import java.util.Scanner;
 
 public class Calculator {
-	static int getFirstValue(Scanner scanner) {
-		System.out.println("첫 번째 입력 값 : ");
-		int first = scanner.nextInt();
-		System.out.println(first);
-		return first;
-	}
-	
-	static int getSecondValue(Scanner scanner) {
-		System.out.println("두번째 입력 값 : ");
-		int second = scanner.nextInt();
-		System.out.println(second);
-		return second;
-	}
-	
-	static String getSymbol(Scanner scanner) {
-		System.out.println("사칙연산 기호 : ");
-		String symbol = scanner.next();
-		System.out.println(symbol);
-		return symbol;
-	}
 
-	static int calculate(int first, String symbol, int second) {
-		int result = 0;
-		if("+".equals(symbol)) {
-			result = first + second;
-			System.out.println("덧셈 : " + result);
-		} else if ("-".equals(symbol)) {
-			result = first - second;
-			System.out.println("뺄셈 : " + result);
-		} else if ("*".equals(symbol)) {
-			result = first * second;
-			System.out.println("곱셈 : " + result);
-		} else if ("/".equals(symbol)) {
-			result = first / second;
-			System.out.println("나눗셈 : " + result);
-		} else {
-			System.out.println("사칙연산 기호가 아닙니다. ");
+	static long calculate(String inputString) {
+		String[] onlyInputValues = Utils.getOnlyValues(inputString);
+		String[] onlyExprs = Utils.getOnlyExprs(inputString);
+		
+		long result = Long.parseLong(onlyInputValues[0]);
+		for(int i = 0; i < onlyExprs.length; i++) {
+			if("+".equals(onlyExprs[i])) {
+				result = result + Long.parseLong(onlyInputValues[i+1]);
+				System.out.println("덧셈 : " + result);
+			} else if ("-".equals(onlyExprs[i])) {
+				result = result - Long.parseLong(onlyInputValues[i+1]);
+				System.out.println("뺄셈 : " + result);
+			} else if ("*".equals(onlyExprs[i])) {
+				result = result * Long.parseLong(onlyInputValues[i+1]);
+				System.out.println("곱셈 : " + result);
+			} else if ("/".equals(onlyExprs[i])) {
+				result = result / Long.parseLong(onlyInputValues[i+1]);
+				System.out.println("나눗셈 : " + result);
+			} else {
+				System.out.println("사칙연산 기호가 아닙니다. ");
+			}
 		}
 		return result;
 	}
 	
-	static void print(int result) {
-		System.out.println("실행 결과 : " + result);
-	}
-	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
-		int result = getFirstValue(scanner);
-
-		while (true) {
-			String symbol = getSymbol(scanner);
-			
-			if ("quit".equals(symbol)) {
-				print(result);
-				break;
-			}
-
-			int second = getSecondValue(scanner);
-			
-			result = calculate(result, symbol, second);
-		}
-		
+		String inputString = Input.getInputValues(scanner);
+		long result = calculate(inputString);
+		Output.print(result);
 		scanner.close();
-			
 	}
 }
