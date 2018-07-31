cd88180757afe07113bcc74eaf28409259c1b121
josephkwon <devel.kwon@gmail.com>
2018-07-31 20:46:30 +0900

    scanner close 부분 추가

--- src/Input.java
+++ src/Input.java
@@ -4,6 +4,7 @@ public class Input {
 	public static String getInputString(Scanner scanner) {
 		System.out.println("공백으로 구분된 문자열 형식으로 계산식을 입력하세요. ");
 		String inputString = scanner.nextLine();
+		scanner.close();
 		return inputString;
 	}
 }
