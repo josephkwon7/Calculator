79215bcd63424670c3e2a0e00b9f9c4dddd3c223
josephkwon <devel.kwon@gmail.com>
2018-07-31 15:18:39 +0900

    사칙 연산 기호에 따라 연산 가능하도록 구현

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,18 +7,24 @@ public class Calculator {
 		int first = scanner.nextInt();
 		System.out.println(first);
 		
+		System.out.println("사칙연산 기호 : ");
+		String exp = scanner.next();
+		System.out.println(exp);
+		
 		System.out.println("두 번째 입력 값 : ");
 		int second = scanner.nextInt();
 		System.out.println(second);
 		
-		
-		System.out.println("덧셈 : " + (first + second));
-		System.out.println("뺄셈 : " + (first - second));
-		System.out.println("곱셈 : " + (first * second));
-		System.out.println("나눗셈 : " + (first / second));
-//		System.out.println(String.format(first + " + " + second + " = %d" , first + second));
-//		System.out.println(String.format(first + " - " + second + " = %d" , first - second));
-//		System.out.println(String.format(first + " * " + second + " = %d" , first * second));
-//		System.out.println(String.format(first + " / " + second + " = %d" , first / second));
+		if("+".equals(exp)) {
+			System.out.println("덧셈 : " + (first + second));
+		} else if ("-".equals(exp)) {
+			System.out.println("뺄셈 : " + (first - second));
+		} else if ("*".equals(exp)) {
+			System.out.println("곱셈 : " + (first * second));
+		} else if ("/".equals(exp)) {
+			System.out.println("나눗셈 : " + (first / second));
+		} else {
+			System.out.println("사칙연산 기호가 아닙니다.");
+		}
 	}
 }
